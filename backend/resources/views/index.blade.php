5b1bcdaa6024d2a8abf013cbaae091e6c5e89142
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-08-26 13:08:52 +0800

    new login design and dashboard with backend

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,11 +1,36 @@
 @include('parts._head')
-<!-- This is the Login UI without Backend  -->
-<section class="bg-gray-50 min-h-screen flex items-center justify-center drop-shadow-2xl" >
+
+
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img class="mx-auto h-20 w-auto" src="./image/logo.jpg" alt="Your Company">
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
+  <form method="POST" action="/login" class="flex flex-col gap-1">
+                    @csrf
+                    <input id="username" class="p-2 mt-8 rounded-xl border" type="text" name="username" placeholder="example@gmail.com" value="{{ old('username') }}" required>
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                    <div class="relative">
+                    <input  class="p-2 mt-8 rounded-xl border w-full" type="password" name="password" placeholder="8-16 character" required>  
+                    </div>
+                    <button class="bg-blue rounded-xl text-white py-3 mt-3 shadow-lg shadow-indigo-500/50 hover:scale-110 duration-300 mb-5">Login</button>
+                </form>       
+  </div>
+</div>
+
+<!-- <section class="bg-gray-50 min-h-screen flex items-center justify-center drop-shadow-2xl" >
         
-        <!-- login container starts here -->
-        <div class="bg-gray-100 flex rounded-2xl shadow-[25px_25px_30px_-24px_rgb(0,0,0,0.6)] max-w-3xl p-2">
+
+        <div class="bg-gray-100 flex rounded-2xl shadow-[25px_25px_30px_-24px_rgb(0,0,0,0.6)] p-2  mx-auto px-5">
             
-        <!-- Form Starts here -->
+        
             <div class=" px-5 text-slate drop-shadow-lg">
                 <h2 class="font-bold text-2xl mt-4">Login</h2>
                
@@ -22,14 +47,8 @@
                     <input  class="p-2 mt-8 rounded-xl border w-full" type="password" name="password" placeholder="8-16 character" required>  
                     </div>
                     <button class="bg-blue rounded-xl text-white py-3 mt-3 shadow-lg shadow-indigo-500/50 hover:scale-110 duration-300 mb-5">Login</button>
-                </form> 
-
-             
-                
-            </div>
-
-            <!-- image -->
-           
+                </form>       
+            </div>   
         </div> 
-    </section>
+    </section> -->
 @include('parts._foot')
--- backend/resources/views/parts/_head.blade.php
+++ backend/resources/views/parts/_head.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>One</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/jpg" href="../image/logo.jpg" />
+
 </head>
 <body>
