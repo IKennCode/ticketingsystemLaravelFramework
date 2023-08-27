0a83dc4e5f1d8447f55cb2ae0350fa73628680c9
mdn1gh7 <mdn1gh7@gmail.com>
2023-08-27 11:13:37 +0800

    Fix layout of departments and jobtitles page

--- backend/resources/views/jobtitles.blade.php
+++ backend/resources/views/jobtitles.blade.php
@@ -1,36 +1,30 @@
 @include('parts._head')
-<form action="/jobtitles/save" method="post">
-    @csrf
-    <input type="text" name="jobtitle" id="jobtitle" autofocus required>
-    <select name="department" id="department" required>
-        <option value="">--</option>
-        @foreach($departments as $department)
-            <option value="{{$department->id}}">{{$department->description}}</option>
-        @endforeach
-    </select>
-    <input type="submit" value="Save">
-</form>
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Job Titles</th>
-                <th>Department</th>
-                <th>Status</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($jobtitles as $jobtitle)
-                <tr>
-                    <td>{{$jobtitle->id}}</td>
-                    <td>{{$jobtitle->description}}</td>
-                    <td>{{$jobtitle->fk_department}}</td>
-                    <td>{{$jobtitle->is_active}}</td>
-                    <td><button>view</button></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="flex justify-center items-center h-screen">
+    <div>
+        @include('parts._navbar')
+    </div>
+    <div class="w-full h-full px-4">
+        <div class="bg-gray-200 px-4 py-2">
+            <strong>Job Titles</strong>
+        </div>
+        <div class="p-4">
+            <form action="/jobtitles/save" method="post" class="mb-4">
+                @csrf
+                <div class="flex items-center">
+                    <input type="text" name="jobtitle" id="jobtitle" class="w-1/3 border rounded px-4 py-2 mr-4" placeholder="Job Title" autofocus required>
+                    <select name="department" id="department" class="w-1/3 border rounded px-4 py-2 mr-4" required>
+                        <option value="Department" selected disabled>Department</option>
+                        @foreach($departments as $department)
+                        <option value="{{$department->id}}">{{$department->description}}</option>
+                        @endforeach
+                    </select>
+                    <button class="border rounded px-6 py-2 border rounded bg-gray-400 hover:bg-gray-900 hover:text-white" type="submit" Value="save">Save</button>
+                </div>
+            </form>
+            @include('parts._jobtitlestable')
+        </div>
+    </div>
+    @include('parts._usersnav')
+</div>
 
 @include('parts._foot')
---
+++ backend/resources/views/parts/_jobtitlestable.blade.php
@@ -0,0 +1,22 @@
+<table class="min-w-full table-auto mb-4 border border-2">
+    <thead>
+        <tr class="bg-gray-300">
+            <th class="px-4 py-2 whitespace-nowrap">#</th>
+            <th class="px-4 py-2 whitespace-nowrap">Job Titles</th>
+            <th class="px-4 py-2 whitespace-nowrap">Department</th>
+            <th class="px-4 py-2 whitespace-nowrap">Status</th>
+            <th class="px-4 py-2 whitespace-nowrap"></th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($jobtitles as $jobtitle)
+        <tr class="hover:bg-gray-100">
+            <td class="px-4 py-2 whitespace-nowrap text-center">{{$jobtitle->id}}</td>
+            <td class="px-4 py-2 whitespace-nowrap text-center">{{$jobtitle->description}}</td>
+            <td class="px-4 py-2 whitespace-nowrap text-center">{{$jobtitle->fk_department}}</td>
+            <td class="px-4 py-2 whitespace-nowrap text-center">{{$jobtitle->is_active}}</td>
+            <td class="px-4 py-2 whitespace-nowrap text-center"><button class="px-4 py-1 border rounded bg-gray-400 hover:bg-gray-900 hover:text-white">view</button></td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
