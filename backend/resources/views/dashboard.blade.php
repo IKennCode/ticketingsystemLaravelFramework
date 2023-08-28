861243aacb98024b78d43255b8f5a65dcfbb8501
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-08-28 14:07:42 +0800

    ticketsview

--- backend/resources/views/dashboard.blade.php
+++ backend/resources/views/dashboard.blade.php
@@ -7,52 +7,52 @@
     </div>
 
     <div class="w-full columns-4 p-4 ">
-        <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
-            <div class="px-4 py-2">
-                <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">All Ticket </h1>
-            </div>
+                    <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
+                        <div class="px-4 py-2">
+                            <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">All Ticket </h1>
+                        </div>
 
-        <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-                <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$ticketCount}}</h1>    
-        </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+                            <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$ticketCount}}</h1>    
+                    </div>
 
-        <!--Div that will hold the pie chart-->
-        <div id="chart_div"></div>
-    </div>
-    <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
-            <div class="px-4 py-2">
-                <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">Open Ticket</h1>
-            </div>
+                    <!--Div that will hold the pie chart-->
+                    <div id="chart_div"></div>
+                </div>
+                <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
+                        <div class="px-4 py-2">
+                            <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">Open Ticket</h1>
+                        </div>
 
-        <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-                <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$openTicketCount}}</h1>    
-        </div>
-    </div>
-    <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
-            <div class="px-4 py-2">
-                <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">Resolved Ticket</h1>
-            </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+                            <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$openTicketCount}}</h1>    
+                    </div>
+                </div>
+                <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
+                        <div class="px-4 py-2">
+                            <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">Resolved Ticket</h1>
+                        </div>
 
-        <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-                <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$resolvedTicketCount}}</h1>    
-        </div>
-    </div>
-    <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
-        <div class="px-4 py-2">
-            <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">Cancelled Ticket</h1>
-        </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+                            <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$resolvedTicketCount}}</h1>    
+                    </div>
+                </div>
+                <div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-800">
+                    <div class="px-4 py-2">
+                        <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">Cancelled Ticket</h1>
+                    </div>
 
-        <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-            <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$cancelledTicketCount}}</h1>    
-        </div>
-    </div>
-    </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+                        <h1 class="text-7xl font-bold text-gray-800 uppercase dark:text-white">{{$cancelledTicketCount}}</h1>    
+                    </div>
+                </div>
+                </div>
 
-    <div class="h-full">
-            @include('parts._ticketsnav')
-    </div>
+                <div class="h-full">
+                        @include('parts._ticketsnav')
+                </div>
 
-    <div id="chart_div"></div>
+                <div id="chart_div"></div>
     
 </div>
 
--- backend/resources/views/tickets.blade.php
+++ backend/resources/views/tickets.blade.php
@@ -5,14 +5,14 @@
     </div>
     <div class="w-full h-screen">
         <div class="bg-gray-200 px-4 py-2">
-            <strong>Tickets</strong>
+            <strong>Tickets</strong> | <a class="px-4 py-1 border rounded bg-gray-400 hover:bg-gray-900 hover:text-white" href="/tickets/createticket">Create Ticket</a>
         </div>
         <div class="p-2">
             <form action="/tickets/search" method="post" class="mb-4">
                 @csrf
                 <div class="flex items-center">
-                    <input type="text" class="w-1/3 border rounded px-2 py-1 mr-1" placeholder="Search" name="search">
-                    <button class="border rounded px-2 py-1 bg-gray-400 hover:bg-gray-900 hover:text-white" type="submit"><i class="bi bi-search"></i></button>
+                    <input type="text" class="w-1/3 border rounded px-4 py-2 mr-4" placeholder="Search" name="search">
+                    <button class="border rounded px-6 py-2 bg-gray-400 hover:bg-gray-900 hover:text-white" type="submit">Go</button>
                 </div>
             </form>
             @include('parts._ticketstable')
