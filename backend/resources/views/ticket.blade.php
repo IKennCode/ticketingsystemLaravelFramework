70b191465dd493f7387c0429e56335b75df724ea
mdn1gh7 <mdn1gh7@gmail.com>
2023-08-29 22:18:38 +0800

    Adjust colors

--- backend/resources/views/ticket.blade.php
+++ backend/resources/views/ticket.blade.php
@@ -1,153 +1,152 @@
 @include('parts._head')
 <div class=" flex justify-center h-screen">
-        <div class="w-64">
-            @include('parts._navbar')
+    <div class="w-64">
+        @include('parts._navbar')
+    </div>
+    <div class="w-full h-screen bg-gray-400">
+        <div class="bg-gray-700 px-4 py-2 text-white">
+            <i class="bi bi-person-hearts"></i> &nbsp; <strong>Ticket View</strong>
         </div>
-        <div class="w-full h-screen bg-gray-600">
-            <div class="bg-gray-700 px-4 py-2 text-white">
-                <i class="bi bi-person-hearts"></i> &nbsp; <strong>Ticket View</strong>
-            </div>
-            
-            <div class="p-2">
-                        <div class="">
-                            @foreach($ticket as $data)
-                            <div class="">
-                                <div class="w-full mb-4">
-                                    <ul class="  w-full mb-4">
-                                        <li class=" text-gray-50 font-extrabold">
-                                            <strong></strong>
-                                        </li>
-                                        <li class=" text-gray-50 text-xl font-bold">
-                                            Ticket ID: <li class=" text-amber-200 text-xl font-bold">{{$data->id}}</li>
-                                        </li>
-                                        <li class=" w-full mb-4" style="display:{{$data->acknowledged_by||$data->status==3||$data->status==4||$data->status==5 ? 'none' : 'block' }};">
-                                            <form action="/tickets/acknowledge" method="post" class="d-flex">
-                                                @csrf
-                                                <input type="hidden" name="ticket_id" value="{{$data->id}}">
-                                                <button type="submit" class="btn btn-sm btn-success">Acknowledge</button>
-                                            </form>
-                                        </li>
-                                        <li class="nav-item px-1" style="display:{{$data->status==5||$data->status==4||$data->status==3 ? 'none' : 'block' }};">
-                                            <form action="/tickets/cancel" method="post" class="d-flex">
-                                                @csrf
-                                                <input type="hidden" name="ticket_id" value="{{$data->id}}">
-                                                <button type="submit" class="btn btn-sm btn-danger">Cancel</button>
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="flex">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Title</span>
-                                                <input type="text" class="form-control" value="{{$data->title}}" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
 
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Description</span>
-                                                <textarea class="form-control" value="{{$data->category}}" disabled>
-                                                    {{$data->description}}
-                                                </textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Status</span>
-                                                <input type="text" class="form-control" value="{{$data->status}}" disabled>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-4">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Created By</span>
-                                                <input type="text" class="form-control" value="{{$data->created_by}}" disabled>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-4">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Date Created</span>
-                                                <input type="text" class="form-control" value="{{$data->created_at}}" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row" style="display:{{$data->resolved_by ? 'block' : 'none'}};">
-                                        <div class="col-4">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Resolved by:</span>
-                                                <input type="text" class="form-control" value="{{$data->resolved_by}}" disabled>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-4">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Resolved date:</span>
-                                                <input type="text" class="form-control" value="{{$data->resolved_at}}" disabled>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-4">
-                                            {{-- <div class="input-group mb-3 input-group-sm">
-                                                <span class="input-group-text">Date Created</span>
-                                                <input type="text" class="form-control" value="{{$data->created_at}}" disabled>
-                                            </div> --}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
-
-                            <div class="flex">
-                                <div class="card-header"><strong>Message</strong></div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-3 m-3">
-                                            <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
-                                            <button class="btn btn-primary btn-sm">Send</button>
-                                        </div>
-                                        <div class="col-8">
-                                            <em><strong>User</strong>: Testing</em><br>
-                                            <strong>User</strong>: Response 1<br>
-                                            <em><strong>User</strong>: Testing</em><br>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card mt-3">
-                                <div class="card-header"><strong>Resolution</strong></div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <form action="/tickets/resolve" method="post">
-                                            @csrf
-                                            @foreach($ticket as $data)
-                                            <div class="col-12">
-                                                <input type="hidden" name="ticket_id" value="{{$data->id}}">
-                                                <textarea class="form-control" name="resolution" id="resolution" cols="10" rows="5"
-                                                    {{$data->status==3||$data->status==4||$data->status==5 ? "disabled" : ""}}
-                                                >{{$data->resolution}}</textarea>
-                                                <button class="btn btn-primary btn-sm" {{$data->status==3||$data->status==4||$data->status==5 ? "disabled" : ""}}>Resolve</button>
-                                            </div>
-                                            @endforeach
-                                        </form>
-                                    </div>
+        <div class="p-2">
+            <div class="">
+                @foreach($ticket as $data)
+                <div class="">
+                    <div class="w-full mb-4">
+                        <ul class="  w-full mb-4">
+                            <li class=" text-gray-50 font-extrabold">
+                                <strong></strong>
+                            </li>
+                            <li class=" text-gray-50 text-xl font-bold">
+                                Ticket ID:
+                            <li class=" text-amber-200 text-xl font-bold">{{$data->id}}</li>
+                            </li>
+                            <li class=" w-full mb-4" style="display:{{$data->acknowledged_by||$data->status==3||$data->status==4||$data->status==5 ? 'none' : 'block' }};">
+                                <form action="/tickets/acknowledge" method="post" class="d-flex">
+                                    @csrf
+                                    <input type="hidden" name="ticket_id" value="{{$data->id}}">
+                                    <button type="submit" class="btn btn-sm btn-success">Acknowledge</button>
+                                </form>
+                            </li>
+                            <li class="nav-item px-1" style="display:{{$data->status==5||$data->status==4||$data->status==3 ? 'none' : 'block' }};">
+                                <form action="/tickets/cancel" method="post" class="d-flex">
+                                    @csrf
+                                    <input type="hidden" name="ticket_id" value="{{$data->id}}">
+                                    <button type="submit" class="btn btn-sm btn-danger">Cancel</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="flex">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Title</span>
+                                    <input type="text" class="form-control" value="{{$data->title}}" disabled>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Description</span>
+                                    <textarea class="form-control" value="{{$data->category}}" disabled>
+                                    {{$data->description}}
+                                    </textarea>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Status</span>
+                                    <input type="text" class="form-control" value="{{$data->status}}" disabled>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Created By</span>
+                                    <input type="text" class="form-control" value="{{$data->created_by}}" disabled>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Date Created</span>
+                                    <input type="text" class="form-control" value="{{$data->created_at}}" disabled>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row" style="display:{{$data->resolved_by ? 'block' : 'none'}};">
+                            <div class="col-4">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Resolved by:</span>
+                                    <input type="text" class="form-control" value="{{$data->resolved_by}}" disabled>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="input-group mb-3 input-group-sm">
+                                    <span class="input-group-text">Resolved date:</span>
+                                    <input type="text" class="form-control" value="{{$data->resolved_at}}" disabled>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                {{-- <div class="input-group mb-3 input-group-sm">
+                                                <span class="input-group-text">Date Created</span>
+                                                <input type="text" class="form-control" value="{{$data->created_at}}" disabled>
+                            </div> --}}
+                        </div>
                     </div>
+                </div>
+            </div>
+            @endforeach
+
+            <div class="flex">
+                <div class="card-header"><strong>Message</strong></div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-3 m-3">
+                            <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
+                            <button class="btn btn-primary btn-sm">Send</button>
+                        </div>
+                        <div class="col-8">
+                            <em><strong>User</strong>: Testing</em><br>
+                            <strong>User</strong>: Response 1<br>
+                            <em><strong>User</strong>: Testing</em><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mt-3">
+                <div class="card-header"><strong>Resolution</strong></div>
+                <div class="card-body">
+                    <div class="row">
+                        <form action="/tickets/resolve" method="post">
+                            @csrf
+                            @foreach($ticket as $data)
+                            <div class="col-12">
+                                <input type="hidden" name="ticket_id" value="{{$data->id}}">
+                                <textarea class="form-control" name="resolution" id="resolution" cols="10" rows="5" {{$data->status==3||$data->status==4||$data->status==5 ? "disabled" : ""}}>{{$data->resolution}}</textarea>
+                                <button class="btn btn-primary btn-sm" {{$data->status==3||$data->status==4||$data->status==5 ? "disabled" : ""}}>Resolve</button>
+                            </div>
+                            @endforeach
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        
-        <div class="w-54">
-        @include('parts._ticketsnav')
-        </div>
-</div>  
+    </div>
+</div>
+
+<div class="w-54">
+    @include('parts._ticketsnav')
+</div>
+</div>
 @include('parts._foot')
--- backend/resources/views/user.blade.php
+++ backend/resources/views/user.blade.php
@@ -1,24 +1,24 @@
 @include('parts._head')
-    <h1>this is the individual users page</h1>
-    @foreach($users as $user)
-        <p>{{$user->id}}</p>
-        <p>{{$user->first_name}}</p>
-        <p>{{$user->middle_name}}</p>
-        <p>{{$user->last_name}}</p>
-        <p>{{$user->birthdate}}</p>
-        <p>{{$user->gender}}</p>
-        <p>{{$user->marital_status}}</p>
-        <p>{{$user->department}}</p>
-        <p>{{$user->job_title}}</p>
-        <p>{{$user->permission}}</p>
-        <p>{{$user->username}}</p>
-        <p>{{$user->status}}</p>
-        <p>
-            <input type="text" placeholder="change_password">
-            <input type="submit" value="Update Password">
-        </p>
-        <p>
-            <button>Reset Password</button>
-        </p>
-    @endforeach
+<h1>this is the individual users page</h1>
+@foreach($users as $user)
+<p>{{$user->id}}</p>
+<p>{{$user->first_name}}</p>
+<p>{{$user->middle_name}}</p>
+<p>{{$user->last_name}}</p>
+<p>{{$user->birthdate}}</p>
+<p>{{$user->gender}}</p>
+<p>{{$user->marital_status}}</p>
+<p>{{$user->department}}</p>
+<p>{{$user->job_title}}</p>
+<p>{{$user->permission}}</p>
+<p>{{$user->username}}</p>
+<p>{{$user->status}}</p>
+<p>
+    <input type="text" placeholder="change_password">
+    <input type="submit" value="Update Password">
+</p>
+<p>
+    <button>Reset Password</button>
+</p>
+@endforeach
 @include('parts._foot')
