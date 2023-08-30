74e3efbddd8251aadf9f7a50581ee09af3180222
mdn1gh7 <mdn1gh7@gmail.com>
2023-08-30 09:35:58 +0800

    Update background colors

--- backend/resources/views/users.blade.php
+++ backend/resources/views/users.blade.php
@@ -4,7 +4,7 @@
         @include('parts._navbar')
     </div>
 
-    <div class="w-full h-screen bg-gray-400">
+    <div class="w-full h-screen bg-gray-600">
         <div class="bg-gray-700 px-4 py-2 text-white">
             <i class="bi bi-person-hearts"></i> &nbsp; <strong>Users</strong>
         </div>
